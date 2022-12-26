1e5912f94d81c1bfe1ba11b616b754c6326f0678
davronkhamdamov <xamdamovdavron6@gmail.com>
2022-12-26 23:48:29 +0500

    work companent added

--- src/Routes/Work/Work.tsx
+++ src/Routes/Work/Work.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
-
+import c from './Work.module.css';
+import amazon from '../../assets/Images/amazon.jpg';
+import { Link } from 'react-router-dom';
 const Work: React.FC = () => {
-  return <div>Work</div>;
+  return (
+    <div className={c.workWrapper}>
+      <div className={c.workItem}>
+        <img src={amazon} alt="" />
+        <h2>Amazon Clone</h2>
+        <div className={c.des}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            molestias quos facilis doloremque labore asperiores velit tempora
+            sapiente, inventore, delectus voluptatem ipsam libero perspiciatis
+            quam veniam.
+          </p>
+          <div className={c.btnGroup}>
+            <Link to="/">Show Clone</Link>
+            <Link to="/">More</Link>
+          </div>
+        </div>
+        <span className={c.workData}>11.12.2022</span>
+      </div>
+      <div className={c.workItem}>
+        <img src={amazon} alt="" />
+        <span className={c.workData}>01.12.2022</span>
+        <h2>Texnomart Clone</h2>
+        <div className={c.des}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            molestias quos facilis doloremque labore asperiores velit tempora
+            sapiente, inventore, delectus voluptatem ipsam libero perspiciatis
+            quam veniam.
+          </p>
+          <div className={c.btnGroup}>
+            <Link to="/">Show Clone</Link>
+            <Link to="/">More</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
