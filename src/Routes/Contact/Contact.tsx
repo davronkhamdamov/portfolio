7c3c83dcb1d28fb98fc17f73e6d4ec7425b39caf
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-02 21:47:43 +0500

    contact page updated

--- src/Routes/Contact/Contact.tsx
+++ src/Routes/Contact/Contact.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import c from "./Contact.module.css";
 const Contact: React.FC = () => {
+  const submitData = (e: any) => {
+    e.preventDefaut();
+    console.log(e);
+  };
   return (
     <div className={c.contactHero}>
       <h1 className={c.title}>Contact</h1>
       <div className={c.formWrap}>
-        <form action="" method="post">
+        <form onSubmit={submitData} method="post">
           <input type="text" placeholder="Your name" required />
           <input type="email" placeholder="Your email" required />
           <textarea name="" cols={40} rows={7} placeholder="Message"></textarea>
