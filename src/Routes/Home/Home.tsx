a731ed41631d7e8254f2987b0e9896a066c09338
davronkhandamov <xamdamovdavron6@gmail.com>
2023-12-24 17:30:01 +0500

    update CV

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import c from './Home.module.css'
-import mainImg from '../../assets/Images/heroBg.png'
-import resume from '../../files/FullStackCV.pdf'
+import React from "react";
+import c from "./Home.module.css";
+import mainImg from "../../assets/Images/heroBg.png";
+import resume from "../../files/CV.pdf";
 const Home: React.FC = () => {
-  return (
-    <main>
-      <div className={c.hero}>
-        <div className={c.heroTextWrap}>
-          <h1 className={c.heroTitle}>HI</h1>
-          <h2 className={c.heroText}>My name is Davron</h2>
-          <p className={c.heroDes}>I'm Frontend Web Developer</p>
-          <a href={resume} className={c.downloadCV} download={resume}>
-            Download My CV
-          </a>
-        </div>
-        <div>
-          <img className={c.heroBg} src={mainImg} alt="" />
-        </div>
-      </div>
-    </main>
-  )
-}
+    return (
+        <main>
+            <div className={c.hero}>
+                <div className={c.heroTextWrap}>
+                    <h1 className={c.heroTitle}>HI</h1>
+                    <h2 className={c.heroText}>My name is Davron</h2>
+                    <p className={c.heroDes}>I'm Frontend Web Developer</p>
+                    <a href={resume} className={c.downloadCV} download={resume}>
+                        Download My CV
+                    </a>
+                </div>
+                <div>
+                    <img className={c.heroBg} src={mainImg} alt="" />
+                </div>
+            </div>
+        </main>
+    );
+};
 
-export default Home
+export default Home;
